8cb993e6b5cdee6647d419fcf00312d72871c317
Farhan1033 <hanzoi1006@gmail.com>
2024-05-18 22:13:14 +0700

    Fixed

--- lib/konten/konten_home.dart
+++ lib/konten/konten_home.dart
@@ -1,7 +1,7 @@
 import 'package:cuci_mobil/menu/booking.dart';
-import 'package:cuci_mobil/konten/options_konten/about.dart';
-import 'package:cuci_mobil/konten/options_konten/review.dart';
 import 'package:cuci_mobil/model/model.dart';
+import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -15,247 +15,192 @@ class Konten extends StatefulWidget {
 }
 
 class _KontenState extends State<Konten> {
-  int _pilihan = 0;
-  List<Widget> _options = [About_konten(), Review_konten()];
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return Scaffold(
-      appBar: AppBar(toolbarHeight: 0),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _headerSection(context, widget.carWash),
-                _infoSection(widget.carWash),
-                _buildTabBar(),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 200,
-                  decoration: BoxDecoration(color: Colors.yellow),
-                  child: _options[_pilihan],
-                ),
-                SizedBox(height: 10),
-                Align(
-                    alignment: Alignment.bottomCenter,
-                    child: _buildBottomButton(context)),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTabBar() {
-    return Container(
-      padding: EdgeInsets.only(right: 200.0),
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 1,
-            blurRadius: 5,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 16.0),
-        child: Row(
-          children: [
-            _buildTabItem("About", 0),
-            _buildTabItem("Review", 1),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTabItem(String title, int index) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            _pilihan = index;
-          });
-        },
-        child: Container(
-          height: 50,
-          decoration: BoxDecoration(
-            color: _pilihan == index
-                ? Color.fromRGBO(139, 219, 154, 1)
-                : Colors.transparent,
-          ),
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                color: _pilihan == index ? Colors.white : Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildBottomButton(BuildContext context) {
-    return Container(
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: GestureDetector(
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => Booking()),
-        ),
-        child: Center(
-          child: Container(
-            height: 100,
-            width: double.infinity,
-            margin: EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(139, 219, 154, 1),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Center(
-              child: Text(
-                "BOOKING NOW",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _headerSection(BuildContext context, CarWash carWash) {
-    return Container(
-      height: 290,
-      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-      child: Row(
+        body: SingleChildScrollView(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          _buildIconButton(
-            Icons.arrow_back_sharp,
-            () => Navigator.pop(context),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                _buildIconButton(Icons.favorite, () {}),
-                const SizedBox(width: 20),
-                _buildIconButton(Icons.share, () {}),
-              ],
-            ),
-          ),
+          _header(context, widget.carWash),
+          _isiKonten(context, widget.carWash)
         ],
       ),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(carWash.imgUrl),
-          fit: BoxFit.cover,
+    ));
+  }
+
+  Widget _header(BuildContext context, CarWash carWash) {
+    return SizedBox(
+      height: 350,
+      width: MediaQuery.of(context).size.width,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.blue,
+            image: DecorationImage(
+                image: NetworkImage(carWash.imgUrl), fit: BoxFit.cover)),
+        child: Padding(
+          padding: EdgeInsets.only(top: 28.0, left: 16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.45),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 220,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Icon(
+                    Icons.share,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _infoSection(CarWash carWash) {
+  Widget _isiKonten(BuildContext context, CarWash carWash) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              _infoCard("Car Washing", Colors.white, FontWeight.bold),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(Icons.star, color: Colors.yellow),
-                    Text(carWash.ratingTempat, style: TextStyle(fontSize: 16)),
-                  ],
+              SizedBox(
+                height: 40,
+                width: 120,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                      child: Text(
+                    "Car Washing",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  )),
                 ),
               ),
+              SizedBox(
+                height: 40,
+                width: 170,
+                child: Container(
+                  child: Center(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                        Text(
+                          carWash.ratingTempat,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
-          SizedBox(height: 10),
           SizedBox(
-            height: 60,
-            width: 300,
+            height: 15,
+          ),
+          Container(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   carWash.namaTempat,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 Text(
                   carWash.alamatTempat,
                   style: TextStyle(fontSize: 16.0),
-                ),
+                )
               ],
             ),
           ),
+          Divider(
+            height: 20,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Deskripsi",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ExpandableText(
+                  carWash.about,
+                  expandText: 'Selengkapnya',
+                  collapseText: 'Sembunyikan',
+                  maxLines: 5,
+                  linkColor: Colors.green,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          )
         ],
-      ),
-    );
-  }
-
-  Widget _infoCard(String title, Color color, FontWeight fontWeight) {
-    return Container(
-      height: 40,
-      width: 120,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(139, 219, 154, 1),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(color: color, fontWeight: fontWeight),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildIconButton(IconData icon, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: 50,
-        width: 50,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Icon(icon),
       ),
     );
   }
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -2,12 +2,14 @@ class CarWash {
   String namaTempat;
   String alamatTempat;
   String ratingTempat;
+  String about;
   String imgUrl;
 
   CarWash({
     required this.namaTempat,
     required this.alamatTempat,
     required this.ratingTempat,
+    required this.about,
     required this.imgUrl,
   });
 }
@@ -17,18 +19,24 @@ List<CarWash> CarWash_List = [
       namaTempat: "Clean Snow Wash",
       alamatTempat: "Jl. Flamboyan I No.2 Rt 03/Rw 09",
       ratingTempat: "5.0 (500 reviewers)",
+      about:
+          " About is a term commonly used to provide information regarding a specific subject, entity, or person. This section typically encompasses various aspects such as history, purpose, mission, and values, offering a comprehensive overview to the reader. The concept of an section has evolved alongside the development of communication mediums. From the early days of printed books and pamphlets to the modern era of digital websites and social media profiles, the essence remains the same: to inform and introduce. Historically, this section was crucial in academic texts, providing context and background to enhance understanding.",
       imgUrl:
           "https://img.freepik.com/free-photo/man-washing-his-car-garage_1157-26046.jpg?t=st=1715603049~exp=1715606649~hmac=d9664ff542aa8e8809a9fdf36d2db054d781b2582d1046f58c642ecdc7b7dd15&w=1060"),
   CarWash(
       namaTempat: "Getwashed Car Wash",
       alamatTempat: "Jl. Jati Asih No.65 Rt 07/Rw 07",
       ratingTempat: "4.5 (400 reviewers)",
+      about:
+          "About is a term commonly used to provide information regarding a specific subject, entity, or person. This section typically encompasses various aspects such as history, purpose, mission, and values, offering a comprehensive overview to the reader. The concept of an section has evolved alongside the development of communication mediums. From the early days of printed books and pamphlets to the modern era of digital websites and social media profiles, the essence remains the same: to inform and introduce. Historically, this section was crucial in academic texts, providing context and background to enhance understanding.",
       imgUrl:
           "https://img.freepik.com/free-photo/professional-washer-blue-uniform-washing-luxury-car-with-water-gun-open-air-car-wash_496169-333.jpg?t=st=1715603154~exp=1715606754~hmac=20b433bad37a82b391775e0093acbad757eb501aecf2b436e984de779bfdd76b&w=1060"),
   CarWash(
       namaTempat: "Berkah Car Wash",
       alamatTempat: "Jl. Sentosa No.18 Rt 03/Rw 05",
       ratingTempat: "4.7 (450 reviewers)",
+      about:
+          "About is a term commonly used to provide information regarding a specific subject, entity, or person. This section typically encompasses various aspects such as history, purpose, mission, and values, offering a comprehensive overview to the reader. The concept of an section has evolved alongside the development of communication mediums. From the early days of printed books and pamphlets to the modern era of digital websites and social media profiles, the essence remains the same: to inform and introduce. Historically, this section was crucial in academic texts, providing context and background to enhance understanding.",
       imgUrl:
           "https://img.freepik.com/free-photo/beautiful-car-washing-service_23-2149212221.jpg?t=st=1715257802~exp=1715261402~hmac=8b9cb8fa19b62d42397c834277357c1f047029870044bbf7ae150377b8b3e330&w=740")
 ];
